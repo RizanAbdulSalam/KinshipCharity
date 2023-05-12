9d36843a13fcf4c2174c34424d704e4147df85b2
RizanAbdulSalam <82836256+RizanAbdulSalam@users.noreply.github.com>
2023-05-12 22:42:05 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -312,3 +312,42 @@ arrows.forEach(icon => {
         setTimeout(() => hideIcons(), 60);
     });
 });
+
+
+
+var loader;
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    }
+    else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function () {
+            loadNow(opacity - 108)
+        }, 5000);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('page-wrapper').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
+window.addEventListener('load', function(){
+	var loader = document.getElementById('loader');
+	setTimeout(function(){
+		loader.style.opacity = '0';
+		setTimeout(function(){
+			loader.style.display = 'none';
+			document.getElementById('content').style.opacity = '0';
+			document.getElementById('content').style.display = 'block';
+			setTimeout(function(){
+				document.getElementById('content').style.opacity = '0.1';
+			}, 500);
+		}, 1000);
+	}, 2000);
+});
